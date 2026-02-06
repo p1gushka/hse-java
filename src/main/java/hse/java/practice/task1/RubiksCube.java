1c59b549fb7842e7443b4dccee50d31efb90540c
p1gushka <sani86188@gmail.com>
2026-02-06 23:35:21 +0300

    added front rotation

--- src/main/java/hse/java/practice/task1/RubiksCube.java
+++ src/main/java/hse/java/practice/task1/RubiksCube.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * Необходимо реализовать интерфейс Cube
  * При повороте передней грани, меняются верх низ право и лево
  */
-public class RubiksCube {
+public class RubiksCube implements Cube {
 
     private static final int EDGES_COUNT = 6;
 
@@ -26,10 +26,152 @@ public class RubiksCube {
         }
     }
 
-    public void front(RotateDirection direction) {
+    private CubeColor[][] copyParts(CubeColor[][] otherParts) {
+        CubeColor[][] parts = new CubeColor[3][3];
+        for (int i = 0; i < 3; i++) {
+            System.arraycopy(otherParts[i], 0, parts[i], 0, 3);
+        }
+        return parts;
+    }
+
+    private void rotateInnerClockwise(EdgePosition position) {
+        CubeColor[][] parts = edges[position.ordinal()].getParts();
+        CubeColor[][] newParts = new CubeColor[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                newParts[2 - j][i] = parts[i][j];
+            }
+        }
+
+        edges[4].setParts(newParts);
+    }
+
+    @Override
+    public void up(RotateDirection direction) {
+        switch (direction) {
+            case CLOCKWISE -> {
+                rotateUpClockwise();
+            }
+            case COUNTERCLOCKWISE -> {
+                rotateUpClockwise();
+                rotateUpClockwise();
+                rotateUpClockwise();
+            }
+        }
+    }
+
+    private void rotateUpClockwise() {
+        rotateInnerClockwise(EdgePosition.UP);
+    }
+
+    @Override
+    public void down(RotateDirection direction) {
+        switch (direction) {
+            case CLOCKWISE -> {
+                rotateDownClockwise();
+            }
+            case COUNTERCLOCKWISE -> {
+                rotateDownClockwise();
+                rotateDownClockwise();
+                rotateDownClockwise();
+            }
+        }
 
     }
-    
+
+    private void rotateDownClockwise() {
+        rotateInnerClockwise(EdgePosition.DOWN);
+    }
+
+    @Override
+    public void left(RotateDirection direction) {
+        switch (direction) {
+            case CLOCKWISE -> {
+                rotateLeftClockwise();
+            }
+            case COUNTERCLOCKWISE -> {
+                rotateLeftClockwise();
+                rotateLeftClockwise();
+                rotateLeftClockwise();
+            }
+        }
+    }
+
+    private void rotateLeftClockwise() {
+        rotateInnerClockwise(EdgePosition.LEFT);
+    }
+
+    @Override
+    public void right(RotateDirection direction) {
+        switch (direction) {
+            case CLOCKWISE -> {
+                rotateRightClockwise();
+            }
+            case COUNTERCLOCKWISE -> {
+                rotateRightClockwise();
+                rotateRightClockwise();
+                rotateRightClockwise();
+            }
+        }
+    }
+
+    private void rotateRightClockwise() {
+        rotateInnerClockwise(EdgePosition.RIGHT);
+    }
+
+    public void front(RotateDirection direction) {
+        switch (direction) {
+            case CLOCKWISE -> {
+                rotateFrontClockwise();
+            }
+            case COUNTERCLOCKWISE -> {
+                rotateFrontClockwise();
+                rotateFrontClockwise();
+                rotateFrontClockwise();
+            }
+        }
+    }
+
+    private void rotateFrontClockwise() {
+        rotateInnerClockwise(EdgePosition.FRONT);
+
+        CubeColor[][] upParts = edges[0].getParts();
+        CubeColor[][] leftParts = edges[2].getParts();
+        CubeColor[][] rightParts = edges[3].getParts();
+        CubeColor[][] downParts = edges[1].getParts();
+
+        CubeColor[][] upPartsCopy = copyParts(upParts);
+        CubeColor[][] leftPartsCopy = copyParts(leftParts);
+        CubeColor[][] rightPartsCopy = copyParts(rightParts);
+        CubeColor[][] downPartsCopy = copyParts(downParts);
+
+        for (int i = 0; i < 3; i++) {
+            upParts[2][i] = leftPartsCopy[i][2];
+            rightParts[i][0] = upPartsCopy[2][i];
+            downParts[0][i] = rightPartsCopy[i][0];
+            leftParts[i][2] = downPartsCopy[0][i];
+        }
+    }
+
+    @Override
+    public void back(RotateDirection direction) {
+        switch (direction) {
+            case CLOCKWISE -> {
+                rotateBackClockwise();
+            }
+            case COUNTERCLOCKWISE -> {
+                rotateBackClockwise();
+                rotateBackClockwise();
+                rotateBackClockwise();
+            }
+        }
+    }
+
+    private void rotateBackClockwise() {
+        rotateInnerClockwise(EdgePosition.BACK);
+    }
+
     public Edge[] getEdges() {
         return edges;
     }
